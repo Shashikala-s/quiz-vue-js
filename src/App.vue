690bc27c9dba5610b0619385ce722c72f910fec9
Shashikala <shashikala.sewwandi@arimaclanka.com>
2024-05-26 10:53:34 +0200

    basic UI and function

--- src/App.vue
+++ src/App.vue
@@ -1,92 +1,251 @@
 <script setup>
 
-import {ref,computed}  from 'vue'
-const questions =ref([
-  
-{
-  question:"what is Vue ?",
-  answer: 0,
-  option:[
-    'A',
-    'B',
-    'C'
-  ]
-},
-{
-  question:"what is this ?",
-  answer: 0,
-  option:[
-    'A',
-    'B',
-    'C'
-  ]
-},{
-  question:"what is be ?",
-  answer: 0,
-  option:[
-    'A',
-    'B',
-    'C'
-  ]
-},
+import { ref, computed } from 'vue'
+
+const questions = ref([
+  {
+	question: 'What is Vue?',
+	answer: 0,
+	options: [
+		'A framework',
+		'A library',
+		'A type of hat'
+	],
+	selected: null
+  },
+  {
+	question: 'What is Vuex used for?',
+	answer: 2,
+	options: [
+		'Eating a delicious snack',
+		'Viewing things',
+		'State management'
+	],
+	selected: null
+  },
+  {
+	question: 'What is Vue Router?',
+	answer: 1,
+	options: [
+		'An ice cream maker',
+		'A routing library for Vue',
+		'Burger sauce'
+	],
+	selected: null
+  }
 ])
 
-const quizCompleted=ref(false)
-const currentQuestion=ref(0)
-const score=computed(()=>{
-
-  let value=0
-  questions.value.map(q =>{
-    if(q.selected==q.answer){
-      value++
-    }
-  })
-  return value
+const quizCompleted = ref(false)
+const currentQuestion = ref(0)
+const score = computed(() => {
+	let value = 0
+	questions.value.map(q => {
+		if (q.selected != null && q.answer == q.selected) {
+			console.log('correct');
+			value++
+		}
+	})
+	return value
 })
 
-const getCurrentQuestion=computed(()=>{
-  let question=questions.value[currentQuestion.value]
-  question.index=currentQuestion.value
-  return question
+const getCurrentQuestion = computed(() => {
+	let question = questions.value[currentQuestion.value]
+	question.index = currentQuestion.value
+	return question
 })
 
-const setAnswer=evt =>{
-  questions.value[currentQuestion.value].selected=evt.taregt.value
-evt.taregt.value=null
+const SetAnswer = (e) => {
+	questions.value[currentQuestion.value].selected = e.target.value
+	e.target.value = null
 }
 
-const nextQuestion=()=>{
-  if(currentQuestion.value<questions.length-1){
-    currentQuestion.value++
-  }else{
-    quizCompleted.value==true
-  }
+const NextQuestion = () => {
+	if (currentQuestion.value < questions.value.length - 1) {
+		currentQuestion.value++
+		return
+	}
+	
+	quizCompleted.value = true
 }
 </script>
 <template>
-  <main class="app">
-  </main>  <h1>Quiz</h1>
-  <section class=""quiz>
-    <div class=""quiz-info>
-      <span class="question">{{ getCurrentQuestion.question }}</span>
-      <span class="question">{{ score }}/{{ questions.length }}</span>
-   
-      <div class="option">
-        <label>
-          <input type="radio"
-          :name="getCurrentQuestion.index"
-          :value="index">
-        </label>
-      </div>
-    </div>
-  </section>
+	<main class="app">
+		<h1>THE QUIZ</h1>
+		
+		<section class="quiz" v-if="!quizCompleted">
+			<div class="quiz-info">
+				<span class="question">{{ getCurrentQuestion.question }}</span>
+				<span class="score">Score {{ score }}/{{ questions.length }}</span>
+			</div>
+			
+			<div class="options">
+				<label 
+					v-for="(option, index) in getCurrentQuestion.options" 
+					:for="'option' + index" 
+					:class="`option ${
+						getCurrentQuestion.selected == index 
+							? index == getCurrentQuestion.answer 
+								? 'correct' 
+								: 'wrong'
+							: ''
+					} ${
+						getCurrentQuestion.selected != null &&
+						index != getCurrentQuestion.selected
+							? 'disabled'
+							: ''
+					}`">
+					<input 
+						type="radio" 
+						:id="'option' + index" 
+						:name="getCurrentQuestion.index" 
+						:value="index" 
+						v-model="getCurrentQuestion.selected" 
+						:disabled="getCurrentQuestion.selected"
+						@change="SetAnswer" 
+					/>
+					<span>{{ option }}</span>
+				</label>
+			</div>
+			
+			<button 
+				@click="NextQuestion" 
+				:disabled="!getCurrentQuestion.selected">
+				{{ 
+					getCurrentQuestion.index == questions.length - 1 
+						? 'Finish' 
+						: getCurrentQuestion.selected == null
+							? 'Select an option'
+							: 'Next question'
+				}}
+			</button>
+		</section>
+
+		<section v-else>
+			<h2>You have finished the quiz!</h2>
+			<p>Your score is {{ score }}/{{ questions.length }}</p>
+		</section>
+	</main>
 </template>
 
 
 <style>
-*{
-  margin: 0;
-  padding: auto;
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	font-family: 'Montserrat', sans-serif;
+}
+
+body {
+	background-color:black;
+	color: #FFF;
+}
+
+.app {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding: 2rem;
+	height: 100vh;
+}
+
+h1 {
+  color: gray;
+  font-weight: 900;
+	font-size: 3rem;
+	margin-bottom: 2rem;
+}
+
+.quiz {
+  	border-radius: 1rem;
+	background-color: white;
+	padding: 2rem;
+	width: 100%;
+	/* max-width: 1640px; */
+  min-width: 500px;
+}
+
+.quiz-info {
+	display: flex;
+	justify-content: space-between;
+	margin-bottom: 1rem;
+}
+
+.quiz-info .question {
+	color: #6b6767;
+	font-size: 1.25rem;
+}
+
+.quiz-info.score {
+	color: #382a2a;
+	font-size: 1.25rem;
+}
+
+.options {
+	margin-bottom: 1rem;
+}
+
+.option {
+	padding: 1rem;
+	display: block;
+	background-color: #413c47;
+	margin-bottom: 0.5rem;
+	border-radius: 0.5rem;
+	cursor: pointer;
+}
+
+.option:hover {
+	background-color: #2d213f;
+}
+
+.option.correct {
+	background-color: #2cce7d;
+}
+
+.option.wrong {
+	background-color: #ff5a5f;
+}
+
+.option:last-of-type {
+	margin-bottom: 0;
+}
+
+.option.disabled {
+  background-color: #000000;
+	opacity: 0.5;
+}
+
+.option input {
+	display: none;
+}
+
+button {
+	appearance: none;
+	outline: none;
+	border: none;
+	cursor: pointer;
+	padding: 0.8rem 2rem;
+	background-color: #e5ac1c;
+	color: #ffffff;
+	font-weight: 700;
+	text-transform: uppercase;
+	font-size: 1.0rem;
+	border-radius: 1rem;
+}
+
+button:disabled {
+	opacity: 0.5;
+}
+
+h2 {
+	font-size: 2rem;
+	margin-bottom: 2rem;
+	text-align: center;
+}
+
+p {
+	color: #e5ac1c;
+	font-size: 1.5rem;
+	text-align: center;
 }
 </style>
-
